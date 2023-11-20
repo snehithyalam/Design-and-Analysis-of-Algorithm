5c2dd949ac83917ca0fb870ab42a06db96ba629c
Snehith Yalam <132338240+snehithyalam@users.noreply.github.com>
2023-11-20 13:21:57 +0530

    Add files via upload

---
+++ armstrong num.cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <math.h>
+
+int isArmstrong(int num) {
+    int originalNum, remainder, n = 0, result = 0;
+
+    originalNum = num;
+
+    while (originalNum != 0) {
+        originalNum /= 10;
+        ++n;
+    }
+
+    originalNum = num;
+
+    while (originalNum != 0) {
+        remainder = originalNum % 10;
+        result += pow(remainder, n);
+        originalNum /= 10;
+    }
+
+    if (result == num)
+        return 1;
+    else
+        return 0;
+}
+
+int main() {
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (isArmstrong(num))
+        printf("%d is an Armstrong number.\n", num);
+    else
+        printf("%d is not an Armstrong number.\n", num);
+
+    return 0;
+}
